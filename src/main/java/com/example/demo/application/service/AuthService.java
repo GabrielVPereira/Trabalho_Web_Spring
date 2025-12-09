7a6a96dd538458e0486df8ffb305f7585ced9a67
GabrielVPereira <167926476+GabrielVPereira@users.noreply.github.com>
2025-12-09 17:57:17 -0300

    Update AuthService.java

--- src/main/java/com/example/demo/application/service/AuthService.java
+++ src/main/java/com/example/demo/application/service/AuthService.java
@@ -5,7 +5,7 @@ import com.example.demo.application.dto.RegisterDTO;
 import com.example.demo.application.dto.TokenDTO;
 import com.example.demo.application.mapper.UserMapper;
 import com.example.demo.config.JwtService;
-import com.example.demo.config.UserDetailsServiceCustom; // Importe o service
+import com.example.demo.config.UserDetailsServiceCustom; 
 import com.example.demo.domain.entity.User;
 import com.example.demo.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,16 +25,16 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final UserMapper userMapper;
     
-    // Injeção do UserDetailsServiceCustom para carregar UserDetails
+   
     private final UserDetailsServiceCustom userDetailsService; 
 
     public TokenDTO register(RegisterDTO dto) {
         User user = userMapper.toEntity(dto);
 
-        // Aplica hash na senha
+       
         user.setPasswordHash(passwordEncoder.encode(dto.getPassword()));
         
-        // Define ou mantém a Role
+        
         if (dto.getRole() == null || dto.getRole().isEmpty()) {
             user.setRole("ROLE_USER");
         } else {
@@ -43,7 +43,7 @@ public class AuthService {
 
         userRepository.save(user);
 
-        // >> CORREÇÃO: Carrega o UserDetails para a geração correta do Token (com roles)
+       
         UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
         
         var jwtToken = jwtService.generateToken(userDetails);
@@ -52,7 +52,7 @@ public class AuthService {
     }
 
     public TokenDTO login(LoginDTO dto) {
-        // Tenta autenticar
+        
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         dto.getEmail(),
@@ -60,7 +60,7 @@ public class AuthService {
                 )
         );
 
-        // >> CORREÇÃO: Carrega o UserDetails para a geração correta do Token (com roles)
+        
         UserDetails userDetails = userDetailsService.loadUserByUsername(dto.getEmail());
 
         var jwtToken = jwtService.generateToken(userDetails);
