ab7d77081fbb4d008aab8c79e8d31c267b517f4a
GabrielVPereira <167926476+GabrielVPereira@users.noreply.github.com>
2025-12-09 17:56:23 -0300

    Update UserMapper.java

--- src/main/java/com/example/demo/application/mapper/UserMapper.java
+++ src/main/java/com/example/demo/application/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
 
-    // Converte de DTO de Registro para Entidade (Usado no criar conta)
+   
     public User toEntity(RegisterDTO dto) {
         if (dto == null) {
             return null;
@@ -16,18 +16,16 @@ public class UserMapper {
         User user = new User();
         user.setName(dto.getName());
         user.setEmail(dto.getEmail());
-        // Nota: A senha e a role serão tratadas no Service por segurança
+       
         return user;
     }
 
-    // Converte de Entidade para DTO de Resposta (Para retornar dados do usuário sem
-    // senha)
-    // Você precisará criar o UserDTO simples se ainda não tiver
+
     public UserDTO toDTO(User entity) {
         if (entity == null) {
             return null;
         }
-        // Supondo que você crie um UserDTO com id, name, email e role
+
         return new UserDTO(
                 entity.getId(),
                 entity.getName(),
